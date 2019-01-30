dab9fa89584b5a60f81eff807c15dbab9e91cbd1
Dante_Attano <danteal65@gmail.com>
2019-01-30 19:06:19 +0300

    Commit_Leha
    Added:
    Enemy prefab
    Mazegenerator script
    EnemyFollowing script
    EnemyPathCalculating script

---
+++ Assets/Scripts/Enemy/EnemyPathCalculating.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathCalculating : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public GameObject pathMark;
+    private Vector2[] path;
+    void Start()
+    {
+        pathMark.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int[] CheckWalls()
+    {
+        int[] walls = new int[6];
+        
+            //pathMark.transform.position
+        
+        return walls;
+    }
+
+    
+}
