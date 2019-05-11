c382fd41f65dda13323734de425abb43718f0d25
weirdstoies <daniltatarinof@mail.ru>
2019-05-11 20:04:31 +0300

    First version of a menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
