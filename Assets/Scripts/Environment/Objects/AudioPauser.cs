297094d7aea3207bf190f8caa3cff0865fea39b7
weirdstoies <daniltatarinof@mail.ru>
2019-05-12 13:26:54 +0300

    Added AudioPaser script to Well prefab

---
+++ Assets/Scripts/Environment/Objects/AudioPauser.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class AudioPauser : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _sound;
+
+    private AudioSource _audioSource;
+    
+    public void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _sound;
+        
+        AudioManager.OnAudioStart += StartSound;
+        AudioManager.OnAudioPause += PauseSound;
+        AudioManager.OnAudioResume += ResumeSound;
+        
+        if(!AudioManager.Instance.Paused)
+            _audioSource.Play();
+    }
+    
+    private void StartSound()
+    {
+        _audioSource.Play();
+    }
+    
+
+    private void PauseSound()
+    {
+        _audioSource.Pause();
+    }
+
+    private void ResumeSound()
+    {
+        _audioSource.UnPause();
+    }
+}
--- Assets/Scripts/Environment/Objects/Well.cs
+++ Assets/Scripts/Environment/Objects/Well.cs
@@ -10,7 +10,6 @@ public class Well : MonoBehaviour
     public delegate void OnTriggerAction();
     public static event OnTriggerAction OnTrigger;
 
-   
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
