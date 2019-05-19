356fc8706db3f3d40c74ff3e64cfa4e3001d6a58
kir1251 <golubev1251@gmail.com>
2019-05-19 20:45:34 +0300

    Object spawn system

---
+++ Assets/Scripts/Environment/Object Generation/ObjectPrefabInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPrefabInfo : MonoBehaviour
+{    
+    public int MaxCountInDungeon = 1000;    
+    public int MinCountInDungeon = 0;   
+    public float SpawnProbMultiplier = 1;
+
+
+    public virtual bool Check(BackgroundController.Biome biome, int size, Hexagon owner)
+    {
+        var name = gameObject.name;        
+        
+        return ((name.Substring(0, 5).Equals(ObjectsGenerator.biomePrefix[(int) biome]) || name.Substring(0, 5).Equals(ObjectsGenerator.anyBPrefix)) &&
+                name.Substring(5, 2).Equals(ObjectsGenerator.sizePrefix[size - 1])) && ObjectsGenerator.Instance.GetCountInDungeon(name) < MaxCountInDungeon;
+    }
+
+}
---
+++ Assets/Scripts/Environment/WellPrefabInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellPrefabInfo : ObjectPrefabInfo
+{
+
+    public override bool Check(BackgroundController.Biome biome, int size, Hexagon owner) 
+    {
+        return base.Check(biome, size, owner);
+    }
+}
