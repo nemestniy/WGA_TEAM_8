e24f25daab993ce399954dea5a48dac8299a9188
Centre <bulavin@it-sound.com>
2019-05-26 21:04:32 +0300

    Pause menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -5,19 +5,24 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static event Action OnExit;
-    
+    public AudioClip click;
+
+
     public void GoToMainMenuButton()
     {
+        GetComponent<AudioSource>().PlayOneShot(click);
         SceneManager.LoadScene("MainMenu");
     }
     
     public void QuitButton()
     {
+        GetComponent<AudioSource>().PlayOneShot(click);
         Application.Quit();
     }
     
     public void SkipLvlButton()
     {
+        GetComponent<AudioSource>().PlayOneShot(click);
         if (SceneManager.GetActiveScene().name == "TutorialTestScene")
         {
             SceneManager.LoadScene("ReleaseScene");
@@ -31,6 +36,7 @@ public class PauseMenu : MonoBehaviour
     
     public void RestartLvlButton()
     {
+        GetComponent<AudioSource>().PlayOneShot(click);
         SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
     }
 }
