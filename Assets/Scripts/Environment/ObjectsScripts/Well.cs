9ba9f954abebd2ef2d3433724736ef214ee0024a
weirdstoies <daniltatarinof@mail.ru>
2019-05-26 18:22:12 +0300

    Enemy pause fix

--- Assets/Scripts/Environment/ObjectsScripts/Well.cs
+++ Assets/Scripts/Environment/ObjectsScripts/Well.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 public class Well : MonoBehaviour
 {
-    [SerializeField]
-    private float _energyContains = 50;
-    
     public static event Action OnTrigger;
 
     private void OnTriggerEnter2D(Collider2D other)
